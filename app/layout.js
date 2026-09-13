bc5def2f2fa38b5340ea31a32a0de699374465f1
familiadelafuente1942-afk <familiadelafuente1942@gmail.com>
2026-09-13 19:38:00 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'NO SE QUIEN SOY',
+  description: 'Archivo biográfico privado'
+}
+
+export default function RootLayout({ children }) {
+  return <html lang="es"><body>{children}</body></html>
+}
